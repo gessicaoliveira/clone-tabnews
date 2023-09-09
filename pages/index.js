0ec9b716cc8d0670c1c0cad60966dee523de7c97
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-09-09 16:08:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
     function Home() {
-        return <h1>Andrey amo você. Se você me ama, dá uma risadinha!!!</h1>
+        return <h1>Andrey eu amo você. Se você me ama, dá uma risadinha!!!</h1>
     }
 
     export default Home;
