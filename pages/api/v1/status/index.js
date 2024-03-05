06a2e7f72f1d734e9536444057f39535dba0a9c2
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2024-03-05 22:43:50 +0000

    add `database.js` and `.en` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
-  response.status(200).json({"Chave":"alunos do curso.dev são pessoa acima da média"});
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result);
+  response
+    .status(200)
+    .json({ Chave: "alunos do curso.dev são pessoa acima da média" });
 }
 
 export default status;
