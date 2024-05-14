de52fa6d9e45e38daf7ef2ac7ac4651338cf26ab
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2024-05-14 13:21:31 +0000

    handles database ssl connection

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Atualizações em breve! 26-01-2024</h1>;
+  return (
+    <>
+      <div>
+        <h1>Atualizações em breve! 26-01-2024</h1>
+      </div>
+    </>
+  );
 }
 
 export default Home;
